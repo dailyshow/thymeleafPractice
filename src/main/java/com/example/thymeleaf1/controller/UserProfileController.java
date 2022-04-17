18fc64c603a37384f608f8aa3bf1d1efaa3127fe
cis <chals001@naver.com>
2022-04-17 17:14:29 +0900

    1. mapper 인터페이스 내용 추가 2. UserProfileController 에 map 으로 되어있는 내용을 전부 다 mapper 로 사용하도록 수정

--- src/main/java/com/example/thymeleaf1/controller/UserProfileController.java
+++ src/main/java/com/example/thymeleaf1/controller/UserProfileController.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaf1.controller;
 
+import com.example.thymeleaf1.mapper.UserProfileMapper;
 import com.example.thymeleaf1.model.UserProfile;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,43 +12,53 @@ import java.util.Map;
 
 @RestController
 public class UserProfileController {
+    private UserProfileMapper mapper;
 
-    private Map<String, UserProfile> userMap;
+//    private Map<String, UserProfile> userMap;
 
-    @PostConstruct
+    public UserProfileController(UserProfileMapper mapper) {
+        this.mapper = mapper;
+    }
+
+/*    @PostConstruct
     public void init() {
         userMap = new HashMap<>();
         userMap.put("1", new UserProfile("1", "cis1", "010-1234-1234", "ansan1"));
         userMap.put("2", new UserProfile("2", "cis2", "010-5678-5678", "ansan2"));
         userMap.put("3", new UserProfile("3", "cis3", "010-9876-9876", "ansan3"));
-    }
+    }*/
 
     @GetMapping("/user/{id}")
     public UserProfile getUserProfile(@PathVariable("id") String id) {
-        return userMap.get(id);
+//        return userMap.get(id);
+        return mapper.getUserProfile(id);
     }
 
     @GetMapping("/user/all")
     public List<UserProfile> getUerProfileList() {
-        return new ArrayList<>(userMap.values());
+//        return new ArrayList<>(userMap.values());
+        return mapper.getUserProfileList();
     }
 
     @PutMapping("/user/{id}")
     public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-        UserProfile userProfile = userMap.get(id);
+        /*UserProfile userProfile = userMap.get(id);
         userProfile.setName(name);
         userProfile.setPhone(phone);
-        userProfile.setAddress(address);
+        userProfile.setAddress(address);*/
+        mapper.updateUserProfile(id, name, phone, address);
     }
 
     @PostMapping("/user/{id}")
     public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-        UserProfile userProfile = new UserProfile(id, name, phone, address);
-        userMap.put(id, userProfile);
+        /*UserProfile userProfile = new UserProfile(id, name, phone, address);
+        userMap.put(id, userProfile);*/
+        mapper.insertUserProfile(id, name, phone, address);
     }
 
     @DeleteMapping("/user/{id}")
     public void deleteUserProfile(@PathVariable("id") String id) {
-        userMap.remove(id);
+//        userMap.remove(id);
+        mapper.deleteUserProfile(id);
     }
 }
--- src/main/java/com/example/thymeleaf1/mapper/UserProfileMapper.java
+++ src/main/java/com/example/thymeleaf1/mapper/UserProfileMapper.java
@@ -1,7 +1,26 @@
 package com.example.thymeleaf1.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.example.thymeleaf1.model.UserProfile;
+import org.apache.ibatis.annotations.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Mapper
 public interface UserProfileMapper {
+    @Select("SELECT * FROM UserProfile WHERE id=#{id}")
+    UserProfile getUserProfile(@Param("id") String id);
+
+    @Select("SELECT * FROM UserProfile")
+    List<UserProfile> getUserProfileList();
+
+    @Update("UPDATE UserProfile SET name=#{name}, phone=#{phone}, address=#{address} WHERE id=#{id}")
+    void updateUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    @Insert("INSERT INTO UserProfile VALUES(#{id}, #{name}, #{phone}, #{address})")
+    void insertUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    @Delete("DELETE FROM UserProfile WHERE id=#{id}")
+    void deleteUserProfile(@Param("id") String id);
 }
